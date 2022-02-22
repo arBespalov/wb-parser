9368d22b4e845a52e290267013e3a09582075fd7
buess <artemka.bespalov@gmail.com>
2022-02-22 15:41:48 +0500

    groovy to kotlin script migration

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    id ("com.android.application")
+    id ("kotlin-android")
+    id ("kotlin-kapt")
+    id ("androidx.navigation.safeargs")
+}
+
+android {
+    compileSdk = 30
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        applicationId = "com.automotivecodelab.wbgoodstracker"
+        minSdk = 21
+        targetSdk = 30
+        versionCode = 2
+        versionName = "1.01"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "SERVER_URL",
+            gradleLocalProperties(rootDir).getProperty("SERVER_URL"))
+        buildConfigField("String", "SERVER_CLIENT_ID",
+            gradleLocalProperties(rootDir).getProperty("SERVER_CLIENT_ID"))
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+    dataBinding {
+        isEnabled = true
+    }
+}
+
+dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.room:room-runtime:2.3.0")
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+    annotationProcessor("androidx.room:room-compiler:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.4.0")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    implementation("org.jsoup:jsoup:1.13.1")
+    implementation("com.google.android.gms:play-services-auth:19.2.0")
+//    debugImplementation 'com.squareup.leakcanary:leakcanary-android:2.7'
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
