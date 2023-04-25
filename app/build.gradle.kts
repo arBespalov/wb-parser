a4a9426ed8cf69d6400986585c0324f87ddf81a8
Artem Bespalov <artem.bespalov@saritasa.com>
2023-04-25 16:40:39 +0500

    1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.automotivecodelab.wbgoodstracker"
         minSdk = 21
         targetSdk = 33
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String", "SERVER_URL",
