d5ec0de2d52df6d856c8f3df412a12b3a78b9bb2
Artem Bespalov <88834530+arBespalov@users.noreply.github.com>
2025-04-06 18:32:07 +0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.automotivecodelab.wbgoodstracker"
         minSdk = 21
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.3"
+        versionCode = 9
+        versionName = "1.3.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String", "SERVER_URL",
